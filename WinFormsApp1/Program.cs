040e14be9977344e2aa16b644ff2e4f0c7d62ae4
Juliocroliveir <cro.julio@gmail.com>
2024-11-27 03:05:13 -0300

    Ajuste faltante

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -11,8 +11,7 @@ namespace WinFormsApp1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new Principal());
-            Application.Run(new ListarProduto());
+            Application.Run(new Principal());
         }
     }
 }
